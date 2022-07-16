74334ee0214c7ddfd8fe502968dadaff9b427759
SemahaDundar <semahadundaruyar94@gmail.com>
2022-07-16 18:29:33 +0300

    odev 15 tamamlandı

---
+++ script2.js
@@ -0,0 +1,16 @@
+const fotoNumaralari=[
+    10,20,30,20,
+    10,40,40,30
+];
+
+
+
+let sayi=[12, 25, 4, 35];
+function getRndInteger(min,max) {
+    Math.floor(Math.random() * 100);
+  }
+
+  const random = Math.floor(Math.random() * fotoNumaralari.length);
+console.log(random, fotoNumaralari[random]);
+
+
